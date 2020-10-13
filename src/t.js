f528b21f7673638df663bcdf9a34a5311da4f6c4
Michael Becker <michael@bemit.codes>
2020-10-13 22:32:16 +0200

    fix in-schema translation in example translator;

--- src/t.js
+++ src/t.js
@@ -62,9 +62,9 @@ const dicDE = createMap({
 const tEN = t(dicEN, 'en');
 const tDE = t(dicDE, 'de');
 
-const browserT = (text, context) => {
+const browserT = (text, context, schema) => {
     const locale = window.localStorage.getItem('locale') || navigator.language;
-    return locale === 'de' ? tDE(text, context) : tEN(text, context);
+    return locale === 'de' ? tDE(text, context, schema) : tEN(text, context, schema);
 };
 
 export {browserT}
